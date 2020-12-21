1b2cc2724003cb49ed7983a7c0f3e7dfd9b42d4a
Zhao Hengrun <55481162+ZhaoHengrun@users.noreply.github.com>
2020-12-21 09:01:01 +0800

    Add files via upload

--- tools/calc_psnr.m
+++ tools/calc_psnr.m
@@ -1,21 +1,26 @@
 close all;
 clear;
 
-file_path = 'E:\Project\VideoEnhance\IDCN_Pytorch\results\qf_10\output\'; % input路径
-file_path_gt = 'E:\Project\VideoEnhance\IDCN_Pytorch\results\qf_10\gt\';  % gt路径
-img_path_list = dir(strcat(file_path,'*.png')); %获取该文件夹中所有.jpg格式的图像
+file_path = 'E:\Project\VideoEnhance\IDCN_Pytorch\results\qf_10\output\';       % input路径
+file_path_gt = 'E:\Project\VideoEnhance\IDCN_Pytorch\results\qf_10\gt\';     % gt路径
+channel = 3;                                                                    % 1为只计算亮度，3为RGB平均
+img_path_list = dir(strcat(file_path,'*.png'));                                 %.png格式的图像
 
-img_num = length(img_path_list);%获取图像总数
-psnr_avr = 0;   %psnr平均值
-if img_num > 0 %有满足条件的图像
-    for pn = 1:img_num %逐一读取图像
-        image_name = img_path_list(pn).name;% 图像名
-        i1 =  imread(strcat(file_path,image_name));%读取图像
-        i2 =  imread(strcat(file_path_gt,image_name));%读取图像
-        fprintf('%d %s\n',pn,strcat(file_path,image_name));% 显示正在处理的图像名
+img_num = length(img_path_list);                                                %获取图像总数
+psnr_avr = 0;                                                                   %psnr平均值
+if img_num > 0                                                                  %有满足条件的图像
+    for pn = 1:img_num                                                          %逐一读取图像
+        image_name = img_path_list(pn).name;                                    % 图像名
+        i1 =  imread(strcat(file_path,image_name));                             %读取图像
+        i2 =  imread(strcat(file_path_gt,image_name));
+        fprintf('%d %s\n',pn,strcat(file_path,image_name));                     % 显示正在处理的图像名
         
-%         [psnr_val] = compute_psnr(i1, i2);  % L
-        [psnr_val, psn] = psnr(i1, i2);   % RGB
+        if channel == 1
+            [psnr_val] = compute_psnr(i1, i2);                                  % L
+        end
+        if channel == 3
+            [psnr_val, psn] = psnr(i1, i2);                                     % RGB
+        end
         psnr_avr = psnr_avr + psnr_val;
         fprintf('psnr: %0.4f\n', psnr_val);
     end
@@ -24,5 +29,3 @@ if img_num > 0 %有满足条件的图像
 else
     fprintf('no image');
 end
-
-% psnr_bic = compute_psnr(im_gnd,im_b)
